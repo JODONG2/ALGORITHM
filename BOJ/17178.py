868975e83eadb56443585135163683387513b74a
JODONG2 <whgusehd96@gmail.com>
2021-11-19 13:02:54 +0900

    Docs: sort annotation

--- BOJ/17178.py
+++ BOJ/17178.py
@@ -1,4 +1,4 @@
-#줄서기 
+#줄서기 https://www.acmicpc.net/problem/17178
 line_num = int(input())
 line,seq,wait = [],[],[]
 
@@ -7,7 +7,9 @@ for _ in range(line_num):
     for ticket in line[-1]:
         seq.append(ticket)
 
-seq.sort(key = lambda x : (x[0], int(x[2:])), reverse = True)
+seq.sort(key = lambda x : (x[0], int(x[2:])), reverse = True) 
+# str로 역순 정렬하면 'A-401', 'A-4001', 'A-4', 'A-102'
+# 위와 같이 역순 정렬해야 'A-4001', 'A-401', 'A-102', 'A-4'
 for line_n in line:
     for tick in line_n:
         wait.append(tick)
