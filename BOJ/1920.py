bf0da893f87a5839064f5375071bed44b051d02d
JODONG2 <whgusehd96@gmail.com>
2021-12-25 02:09:55 +0900

    Refactor: ditctionary key don't must have to string

--- BOJ/1920.py
+++ BOJ/1920.py
@@ -3,13 +3,13 @@ di = {}
 input_num_list = list(map(int,input().split()))
 for n in range(N):
     input_num = input_num_list[n]
-    di[f"{input_num}"] = input_num
+    di[input_num] = input_num
 M = int(input())
 compare_num_list = list(map(int,input().split()))
 for m in range(M):
     compare_num = compare_num_list[m]
     try:
-        temp = di[f"{compare_num}"] -1 
+        di[compare_num] 
         print(1)
     except:
         print(0)
